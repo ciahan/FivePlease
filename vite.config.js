421d0de53a605e31f9af4de2cea630390bd4fd5d
Ciara Feng <zihanfeng09@gmail.com>
2026-08-06 00:28:39 -0400

    changed text color

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1', // Forces a clean local network route instead of localhost loopback
+    watch: {
+      usePolling: true, // Forces Vite to manually check for file saves on Mac
+    },
+  },
 })
